9802a0544973ad5f3b22e2d63f976f306e4bda84
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-18 21:12:44 +0900

    リファクタリング

--- tools/99_initialize.sh
+++ tools/99_initialize.sh
@@ -6,13 +6,16 @@ vagrant_dir="${script_dir}/../vagrant"
 # 所定のディレクトリを、仮想マシンへ転送する用意をする
 project_base_dir=$(readlink -f "${script_dir}/..")
 
-env WORKDIR='/cygdrive/w/Vagrant'   \
-    /bin/bash -xue "${vagrant_dir}/setup-userdata.sh"   \
+env     \
+    PROJECT_BASE_DIR="${project_base_dir}"      \
+    WORKDIR='/cygdrive/w/Vagrant'               \
+    /bin/bash -xue "${vagrant_dir}/common/setup-userdata.sh"    \
     || exit $?
 
 pushd "${vagrant_dir}"
 
 time  vagrant destroy -f
+sleep 10
 time  vagrant up ; echo $?
 
 popd
